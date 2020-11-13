75d978bbfcdfccbfdedf578819d58a3d3a5df8a1
Langford-tech <55645613+langfordquarshie21@users.noreply.github.com>
2020-11-14 11:48:19 +1300

    Add files via upload

---
+++ precache-manifest.4779bbb62569485e1df905d9ddd37779.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b04048e401907782868",
+    "url": "/css/app.69ee2f3e.css"
+  },
+  {
+    "revision": "45df6b55966537aaeb32b313f31ea74e",
+    "url": "/img/1.45df6b55.jpg"
+  },
+  {
+    "revision": "e8fe5cb09a26a3fdd3d92336b898a0d9",
+    "url": "/img/2.e8fe5cb0.jpg"
+  },
+  {
+    "revision": "f258baa328a38e681b19a8894680af32",
+    "url": "/img/3.f258baa3.jpg"
+  },
+  {
+    "revision": "db262410c5bd7e9291490727af7f6341",
+    "url": "/img/bookmark.db262410.svg"
+  },
+  {
+    "revision": "a37187f0715b60591108313e208b1bed",
+    "url": "/img/camera.a37187f0.svg"
+  },
+  {
+    "revision": "11a57ec108824114c982329f57ae4371",
+    "url": "/img/heart.11a57ec1.svg"
+  },
+  {
+    "revision": "a6780dd195501253511c28f1a03379cb",
+    "url": "/img/home.a6780dd1.svg"
+  },
+  {
+    "revision": "0dec417928863d43f88a01ec70a89eaf",
+    "url": "/img/instagram-logo.0dec4179.png"
+  },
+  {
+    "revision": "619c3884392a9e0ac6cabb11c5543e91",
+    "url": "/img/message-circle.619c3884.svg"
+  },
+  {
+    "revision": "9985e34938143f9e1a534e307834904a",
+    "url": "/img/plus-square.9985e349.svg"
+  },
+  {
+    "revision": "c912559ed77efe1161c5f7e28d46fd02",
+    "url": "/img/search.c912559e.svg"
+  },
+  {
+    "revision": "fe69416370a2a60812c597a75126168e",
+    "url": "/img/send.fe694163.svg"
+  },
+  {
+    "revision": "2ad3882ffdc1f3587dd4f5a335e035b6",
+    "url": "/img/settings.2ad3882f.svg"
+  },
+  {
+    "revision": "d03aef73f525bef63e5103266f7eec98",
+    "url": "/img/user-plus.d03aef73.svg"
+  },
+  {
+    "revision": "dd6cb04682f69e37b2a16bc550baf155",
+    "url": "/img/user.dd6cb046.svg"
+  },
+  {
+    "revision": "030b49cd4628f2c66491aef1bccf745d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b04048e401907782868",
+    "url": "/js/app.846acc7a.js"
+  },
+  {
+    "revision": "961c12ed4adad61f51ec",
+    "url": "/js/chunk-vendors.2af9c472.js"
+  },
+  {
+    "revision": "b7601f3862a3144c156e69a9dd6d4984",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
